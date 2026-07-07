6947c241e0bcf99fc0adab350203307ad7f5607d
diama toure <diama.toure1@unchk.edu.sn>
2026-07-07 12:26:24 +0000

    ajout des pages

--- pages/prendre_rdv.php
+++ pages/prendre_rdv.php
@@ -7,7 +7,7 @@
 session_start();
 require_once __DIR__ . '/../includes/connexion.php';
 require_once __DIR__ . '/../includes/fonctions.php';
-
+//test
 // Protection : patient connecté uniquement
 if (!isLoggedIn() || getRole() !== 'patient') {
     redirect('login.php?redirect=prendre_rdv.php' . (isset($_GET['medecin']) ? '%3Fmedecin%3D'.$_GET['medecin'] : ''));
--- pages/profil_patient.php
+++ pages/profil_patient.php
@@ -7,7 +7,7 @@
 session_start();
 require_once __DIR__ . '/../includes/connexion.php';
 require_once __DIR__ . '/../includes/fonctions.php';
-
+//test
 // Protection : patient connecté uniquement
 if (!isLoggedIn() || getRole() !== 'patient') {
     redirect('login.php?redirect=profil_patient.php');
